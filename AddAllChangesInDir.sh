7b0c7ca0dbc83fc8f31763a18172eacda5769ea1
Günter Neiß <gneiss@web.de>
2025-05-27 05:30:49 +0200

    x

---
+++ AddAllChangesInDir.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -x
+
+# Funktion: AddAllChangesInDir
+# Syntax: AddAllChangesInDir [-o] [Dir] [CommitMsg]
+only_top_level=false
+dir="."
+msg="Auto-Commit"
+
+# Argumentverarbeitung
+while [[ "$1" =~ ^- ]]; do
+  case "$1" in
+    -o) only_top_level=true ;;
+  esac
+  shift
+done
+
+# Nächstes Argument: Directory?
+if [[ -d "$1" ]]; then
+  dir="$1"
+  shift
+fi
+
+# Commit-Message, falls angegeben
+if [[ "$1" ]]; then
+  msg="$1"
+fi
+
+# Wenn Änderungen vorhanden sind
+if AnyChangesInDir $($only_top_level && echo "-o") "$dir"; then
+  git add "$dir"
+  git commit -m "$msg"
+  git push
+ fi
+
+
+
+# Funktion: AnyChangesInDir
+# Syntax: AnyChangesInDir [-o] [Dir]
+only_top_level=false
+dir="."
+
+# Argumente verarbeiten
+while [[ "$1" ]]; do
+  case "$1" in
+    -o) only_top_level=true ;;
+    *) dir="$1" ;;
+  esac
+  shift
+done
+
+# Prüfe Änderungen im Verzeichnis
+if [ -z "$(git status --porcelain "$dir")" ]; then
+  exit 1
+fi
+if $only_top_level; then
+  git status --porcelain "$dir" | grep -qE "^[ MARCUD?\!]{2} $(basename "$dir")/"
+ else
+  git status --porcelain "$dir" | grep -qE "^[ MARCUD?\!]{2}"
+fi
+
