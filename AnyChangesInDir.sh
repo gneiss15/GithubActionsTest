6ff07e2e89071d1032b04e8f2b8039d42593c933
Günter Neiß <gneiss@web.de>
2025-05-27 05:25:52 +0200

    x

---
+++ AnyChangesInDir.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#set -x
+
+# Funktion: AnyChangesInDir
+# Syntax: AnyChangesInDir [-o] [Dir]
+only_top_level=false
+dir="."
+
+# Argumente verarbeiten
+while [[ "$1" ]]; do
+  case "$1" in
+    -o) only_top_level=true ;;
+    *) dir="$1" ;;
+  esac
+  shift
+done
+
+# Prüfe Änderungen im Verzeichnis
+if [ -z "$(git status --porcelain "$dir")" ]; then
+  exit 1
+fi
+if $only_top_level; then
+  git status --porcelain "$dir" | grep -qE "^[ MARCUD?\!]{2} $(basename "$dir")/"
+ else
+  git status --porcelain "$dir" | grep -qE "^[ MARCUD?\!]{2}"
+fi
+
