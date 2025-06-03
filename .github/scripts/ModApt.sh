c912acc5017793449f3ba6b1a1eaf77b899e9730
Günter Neiß <gneiss@web.de>
2025-06-03 07:15:11 +0200

    x

--- .github/scripts/ModApt.sh
+++ .github/scripts/ModApt.sh
@@ -14,9 +14,14 @@ if [ ! -d "$APT_CACHE_DIR" ]; then
 fi
 sudo ln -s "$APT_CACHE_DIR/archives/" /var/cache/apt/archives
 
-sudo apt update
-sudo apt install -y locales-all
-
+sudo apt-get update
+# locales-all is needed, otherwise it will crash with LANG=ro_RO.UTF-8
+sudo apt-get install -y locales-all git build-essential autoconf cmake \
+  libglu1-mesa-dev libgtk-3-dev libdbus-1-dev libwebkit2gtk-4.1-dev \
+  desktop-file-utils libegl-mesa0 libnss-mdns libglx-mesa0 libglx0 \
+  libxcb-dri2-0 libxcb-dri3-0 libxcb-glx0 libxcb-present0 libxcb-sync1 \
+  libxcb-xfixes0 libxshmfence1 libgl1 libdrm2 libgbm1 libvulkan1 \
+  zlib1g-dev libbz2-dev libboost-all-dev
 
 sudo rm -rf $APT_CACHE_DIR/archives/partial/
 sudo rm -rf $APT_CACHE_DIR/archives/lock
